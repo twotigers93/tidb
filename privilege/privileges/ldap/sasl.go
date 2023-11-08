503c6884a2174b8ae5651d54032daf41987b9515
twotigers <twotigers@test.com>
2023-11-08 17:43:01 +0800

    rm ldap

--- privilege/privileges/ldap/sasl.go
+++ privilege/privileges/ldap/sasl.go
@@ -15,10 +15,6 @@
 package ldap
 
 import (
-	"context"
-
-	"github.com/go-ldap/ldap/v3"
-	"github.com/pingcap/errors"
 	"github.com/twotigers93/tidb/privilege/conn"
 )
 
@@ -30,70 +26,71 @@ type ldapSASLAuthImpl struct {
 
 // AuthLDAPSASL authenticates the user through LDAP SASL
 func (impl *ldapSASLAuthImpl) AuthLDAPSASL(userName string, dn string, clientCred []byte, authConn conn.AuthConn) error {
-	dn, ldapConn, err := func() (string, *ldap.Conn, error) {
-		// It's fine to just `RLock` here, even we're fetching resources from the pool, because the resource pool can be
-		// accessed concurrently. The `RLock` here is to protect the configurations.
-		//
-		// It's a bad idea to lock through the whole function, as this function will write/read to interact with the client.
-		// If the client somehow died and don't send responds, this function will not end for a long time (until connection
-		// timeout) and this lock will not be released. Therefore, we only `RLock` the configurations in the
-		impl.RLock()
-		defer impl.RUnlock()
-
-		var err error
-		if len(dn) == 0 {
-			dn, err = impl.searchUser(userName)
-			if err != nil {
-				return "", nil, err
-			}
-		} else {
-			dn = impl.canonicalizeDN(userName, dn)
-		}
-
-		ldapConn, err := impl.getConnection()
-		if err != nil {
-			return "", nil, errors.Wrap(err, "create LDAP connection")
-		}
-
-		return dn, ldapConn, nil
-	}()
-	if err != nil {
-		return err
-	}
-
-	defer impl.putConnection(ldapConn)
-	for {
-		resultCode, serverCred, err := ldapConn.ServerBindStep(clientCred, dn, impl.saslAuthMethod, nil)
-		if err != nil {
-			return err
-		}
-
-		// The client is still waiting for the last serverCred, so cannot break the loop right now even if the `resultCode`
-		// is 0
-		err = authConn.WriteAuthMoreData(serverCred)
-		if err != nil {
-			return err
-		}
-
-		// In TiDB, all `context.Context` before `dispatch` is all `context.Background()` with some KV used for log, so
-		// it's just fine to use `context.Background()` here.
-		err = authConn.Flush(context.Background())
-		if err != nil {
-			return err
-		}
-
-		// `resultCode == 0` means the result is `LDAP_SUCCESS`, the authentication succeeds and we could go further now.
-		if resultCode == 0 {
-			break
-		}
-
-		clientCred, err = authConn.ReadPacket()
-		if err != nil {
-			return err
-		}
-	}
-
 	return nil
+	//dn, ldapConn, err := func() (string, *ldap.Conn, error) {
+	//	// It's fine to just `RLock` here, even we're fetching resources from the pool, because the resource pool can be
+	//	// accessed concurrently. The `RLock` here is to protect the configurations.
+	//	//
+	//	// It's a bad idea to lock through the whole function, as this function will write/read to interact with the client.
+	//	// If the client somehow died and don't send responds, this function will not end for a long time (until connection
+	//	// timeout) and this lock will not be released. Therefore, we only `RLock` the configurations in the
+	//	impl.RLock()
+	//	defer impl.RUnlock()
+	//
+	//	var err error
+	//	if len(dn) == 0 {
+	//		dn, err = impl.searchUser(userName)
+	//		if err != nil {
+	//			return "", nil, err
+	//		}
+	//	} else {
+	//		dn = impl.canonicalizeDN(userName, dn)
+	//	}
+	//
+	//	ldapConn, err := impl.getConnection()
+	//	if err != nil {
+	//		return "", nil, errors.Wrap(err, "create LDAP connection")
+	//	}
+	//
+	//	return dn, ldapConn, nil
+	//}()
+	//if err != nil {
+	//	return err
+	//}
+	//
+	//defer impl.putConnection(ldapConn)
+	//for {
+	//	resultCode, serverCred, err := ldapConn.ServerBindStep(clientCred, dn, impl.saslAuthMethod, nil)
+	//	if err != nil {
+	//		return err
+	//	}
+	//
+	//	// The client is still waiting for the last serverCred, so cannot break the loop right now even if the `resultCode`
+	//	// is 0
+	//	err = authConn.WriteAuthMoreData(serverCred)
+	//	if err != nil {
+	//		return err
+	//	}
+	//
+	//	// In TiDB, all `context.Context` before `dispatch` is all `context.Background()` with some KV used for log, so
+	//	// it's just fine to use `context.Background()` here.
+	//	err = authConn.Flush(context.Background())
+	//	if err != nil {
+	//		return err
+	//	}
+	//
+	//	// `resultCode == 0` means the result is `LDAP_SUCCESS`, the authentication succeeds and we could go further now.
+	//	if resultCode == 0 {
+	//		break
+	//	}
+	//
+	//	clientCred, err = authConn.ReadPacket()
+	//	if err != nil {
+	//		return err
+	//	}
+	//}
+	//
+	//return nil
 }
 
 // SetSASLAuthMethod sets the authentication method used by SASL
--- privilege/privileges/privileges.go
+++ privilege/privileges/privileges.go
@@ -572,12 +572,12 @@ func (p *UserPrivileges) ConnectionVerification(user *auth.UserIdentity, authUse
 			return info, ErrAccessDenied.FastGenByArgs(user.Username, user.Hostname, hasPassword)
 		}
 	} else if record.AuthPlugin == mysql.AuthLDAPSASL {
-		if err = ldap.LDAPSASLAuthImpl.AuthLDAPSASL(authUser, pwd, authentication, authConn); err != nil {
-			// though the pwd stores only `dn` for LDAP SASL, it could be unsafe to print it out.
-			// for example, someone may alter the auth plugin name but forgot to change the password...
-			logutil.BgLogger().Warn("verify through LDAP SASL failed", zap.String("username", user.Username), zap.Error(err))
-			return info, ErrAccessDenied.FastGenByArgs(user.Username, user.Hostname, hasPassword)
-		}
+		//if err = ldap.LDAPSASLAuthImpl.AuthLDAPSASL(authUser, pwd, authentication, authConn); err != nil {
+		//	// though the pwd stores only `dn` for LDAP SASL, it could be unsafe to print it out.
+		//	// for example, someone may alter the auth plugin name but forgot to change the password...
+		//	logutil.BgLogger().Warn("verify through LDAP SASL failed", zap.String("username", user.Username), zap.Error(err))
+		//	return info, ErrAccessDenied.FastGenByArgs(user.Username, user.Hostname, hasPassword)
+		//}
 	} else if record.AuthPlugin == mysql.AuthLDAPSimple {
 		if err = ldap.LDAPSimpleAuthImpl.AuthLDAPSimple(authUser, pwd, authentication); err != nil {
 			logutil.BgLogger().Warn("verify through LDAP Simple failed", zap.String("username", user.Username), zap.Error(err))
